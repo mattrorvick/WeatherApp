79f421591f6a6b781fea651243b7016ae7530c56
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-05 11:06:35 -0600

    adding error if zip code is wrong

--- src/main/java/com/tts/weatherapp/service/WeatherService.java
+++ src/main/java/com/tts/weatherapp/service/WeatherService.java
@@ -1,9 +1,10 @@
 package com.tts.weatherapp.service;
 
-import com.tts.weatherapp.models.Response;
+import com.tts.weatherapp.model.Response;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -19,9 +20,15 @@ public class WeatherService {
 
         String url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + "&appid=" + apiKey;
         RestTemplate restTemplate = new RestTemplate();
-        
+
         //shove the response in a response "object"
-        return restTemplate.getForObject(url, Response.class);
+        try {
+            return restTemplate.getForObject(url, Response.class);
+        } catch(HttpClientErrorException exception) {
+            Response response = new Response();
+            response.setName("ERROR");
+            return response;
+        }
     }
 
 
