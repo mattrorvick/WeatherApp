31b6fbc62a2b0207b7a1abd0caf4333685f6bd08
Matt Rorvick <mr3020@Matts-MacBook-Pro-3.local>
2020-11-08 21:05:44 -0600

    adjusting for zipcode searching

--- src/main/java/com/tts/weatherapp/controller/WeatherController.java
+++ src/main/java/com/tts/weatherapp/controller/WeatherController.java
@@ -2,6 +2,7 @@ package com.tts.weatherapp.controller;
 
 import com.tts.weatherapp.model.Request;
 import com.tts.weatherapp.model.Response;
+import com.tts.weatherapp.model.Storage;
 import com.tts.weatherapp.service.WeatherService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,21 +17,29 @@ public class WeatherController {
     @Autowired
     private WeatherService weatherService;
 
+    
+
 
     @GetMapping("/")
     public String getIndex(Model model) {
         
-        
+        Iterable<Storage> searches = weatherService.getRecentSearch();
+
         model.addAttribute("request", new Request());
+        model.addAttribute("searches", searches);
         return "index";
     }
 
     @PostMapping("/")
     public String postIndex(Request request, Model model) {
 
+
+        Iterable<Storage> searches = weatherService.getRecentSearch();
+
         Response data = weatherService.getForecast(request.getZipCode());
 
         model.addAttribute("data", data);
+        model.addAttribute("searches", searches);
 
         return "index";
     }
